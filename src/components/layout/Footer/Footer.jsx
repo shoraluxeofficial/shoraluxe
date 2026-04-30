fecce22d91cceccac874db8674ffed642f81904f
balajiprojects049 <balajiprojects049@gmail.com>
2026-04-30 17:59:52 +0530

    Make footer social icons larger and stable with original brand colors

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -37,10 +37,10 @@ const Footer = () => {
             <h4 className="f-col-title">About the Brand</h4>
             <p className="f-brand-desc">Shoraluxe is a tribute to timeless beauty, blending ancient rituals with modern dermatology. Our formulations are crafted for results, designed for luxury.</p>
             <div className="f-social-aura">
-              <a href="https://www.instagram.com/shora_luxe?igsh=MXhnNW82ZG0zbHVoaQ==" target="_blank" rel="noopener noreferrer" className="f-social-link instagram"><FaInstagram size={18} /></a>
-              <a href="https://www.facebook.com/profile.php?id=61580485915665&sk=directory_links" target="_blank" rel="noopener noreferrer" className="f-social-link facebook"><FaFacebook size={18} /></a>
-              <a href="https://youtube.com/@shoraluxe?si=nWwGVUSLCTnNGWyz" target="_blank" rel="noopener noreferrer" className="f-social-link youtube"><FaYoutube size={18} /></a>
-              <a href="https://www.threads.net/@shora_luxe" target="_blank" rel="noopener noreferrer" className="f-social-link threads"><SiThreads size={18} /></a>
+              <a href="https://www.instagram.com/shora_luxe?igsh=MXhnNW82ZG0zbHVoaQ==" target="_blank" rel="noopener noreferrer" className="f-social-link instagram"><FaInstagram size={28} /></a>
+              <a href="https://www.facebook.com/profile.php?id=61580485915665&sk=directory_links" target="_blank" rel="noopener noreferrer" className="f-social-link facebook"><FaFacebook size={28} /></a>
+              <a href="https://youtube.com/@shoraluxe?si=nWwGVUSLCTnNGWyz" target="_blank" rel="noopener noreferrer" className="f-social-link youtube"><FaYoutube size={28} /></a>
+              <a href="https://www.threads.net/@shora_luxe" target="_blank" rel="noopener noreferrer" className="f-social-link threads"><SiThreads size={28} /></a>
             </div>
             <div className="f-contact-info">
               <a href="mailto:contact@shoraluxe.com" className="f-contact-link">contact@shoraluxe.com</a>
